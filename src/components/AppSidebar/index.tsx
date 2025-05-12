85de830e452b864793f38d050d5dfcfc94caa690
Yili <oz841119@gmail.com>
2025-05-12 22:23:29 +0800

    style: adjust side bar style.

--- src/components/AppSidebar/index.tsx
+++ src/components/AppSidebar/index.tsx
@@ -1,5 +1,4 @@
 import { EXAMS } from "@/configs/exams";
-import Image from "next/image";
 import {
 	Sidebar,
 	SidebarContent,
@@ -7,35 +6,35 @@ import {
 	SidebarGroup,
 	SidebarGroupLabel,
 	SidebarHeader,
-	SidebarMenu,
-	SidebarMenuButton,
-	SidebarMenuItem,
 } from "../shadcn-ui/sidebar";
 import Logo from "../Logo";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 export const AppSidebar = () => {
+	const pathname = usePathname();
+	const isActive = (path: string) => pathname === path;
 	return (
 		<Sidebar>
 			<SidebarHeader className="flex items-center gap-3 px-4 py-3 border-b">
 				<Logo />
 			</SidebarHeader>
 			<SidebarContent>
-				<SidebarGroup />
 				<SidebarGroup>
 					<SidebarGroupLabel>EXAM</SidebarGroupLabel>
-					<SidebarContent>
-						<SidebarMenu>
-							<SidebarMenuItem>
-								{EXAMS.map((exam) => (
-									<SidebarMenuButton key={exam.routeParam}>
-										<a href={`/dashboard/exam/${exam.routeParam}`}>
-											{exam.menuLabel}
-										</a>
-									</SidebarMenuButton>
-								))}
-							</SidebarMenuItem>
-						</SidebarMenu>
-					</SidebarContent>
+						<ul className="flex flex-col">
+							{EXAMS.map((exam) => (
+								<li
+									key={exam.routeParam}
+									className={cn("text-sm px-2 rounded-md", isActive(`/dashboard/exam/${exam.routeParam}`) ? "bg-foreground/10" : "hover:bg-foreground/5")}
+								>
+									<Link href={`/dashboard/exam/${exam.routeParam}`} className="block py-2">
+										{exam.menuLabel}
+									</Link>
+								</li>
+							))}
+						</ul>
 				</SidebarGroup>
 			</SidebarContent>
 			<SidebarFooter />

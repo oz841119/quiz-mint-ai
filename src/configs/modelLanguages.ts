c800c72e666b2abb8729627470018ebe9402fc47
Yili <oz841119@gmail.com>
2025-05-17 16:27:57 +0800

    chore: add more languages

--- src/configs/modelLanguages.ts
+++ src/configs/modelLanguages.ts
@@ -4,11 +4,39 @@ export const MODEL_LANGUAGES = [
     label: "繁體中文",
   },
   {
+    value: "zh-CN",
+    label: "简体中文",
+  },
+  {
     value: "en",
     label: "English",
   },
   {
     value: "ja",
     label: "日本語",
+  },
+  {
+    value: "ko",
+    label: "한국어",
+  },
+  {
+    value: "fr",
+    label: "Français",
+  },
+  {
+    value: "de",
+    label: "Deutsch",
+  },
+  {
+    value: "es",
+    label: "Español",
+  },
+  {
+    value: "ru",
+    label: "Русский",
+  },
+  {
+    value: "pt",
+    label: "Português",
   },
 ];
